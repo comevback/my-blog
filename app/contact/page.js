fe11e1794aeb6d13575250568cea092d0c3c8298
comevback <xuxiang5012@gmail.com>
2024-07-17 14:45:12 +0900

    on building

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,10 @@
 const AboutPage = () => {
     return (
         <div className="bg-black h-[100vh] flex justify-center items-center flex-col gap-8" >
-            <h1 className="text-8xl font-bold text-gray-100">About</h1>
+            <h1 className="text-8xl font-bold text-gray-100">Contact</h1>
             <h2 className="text-4xl font-bold text-gray-100">Still on building......</h2>
         </div >
     );
-};
+}
 
 export default AboutPage;
--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,10 +1,10 @@
-const AboutPage = () => {
+const Gallery = () => {
     return (
         <div className="bg-black h-[100vh] flex justify-center items-center flex-col gap-8" >
-            <h1 className="text-8xl font-bold text-gray-100">About</h1>
+            <h1 className="text-8xl font-bold text-gray-100">Gallery</h1>
             <h2 className="text-4xl font-bold text-gray-100">Still on building......</h2>
         </div >
     );
-};
+}
 
-export default AboutPage;
+export default Gallery;
